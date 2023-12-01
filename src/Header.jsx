391a7de28d55a7c0940a1fdcb6ee5b3a51b3d526
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-11-30 16:06:27 -0800

    added header.jsx

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header>
+      <nav>
+        <a href="#">Home</a> | <a href="#">Signup</a> || <a href="#">Login</a>
+      </nav>
+    </header>
+  );
+}
