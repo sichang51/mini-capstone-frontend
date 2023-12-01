94c8773de14b398715e91285c60e47e8c059695c
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-11-30 17:34:42 -0800

    added image, img sizing, price, and description

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,12 +1,17 @@
 export function ProductsIndex(props) {
+  console.log(props);
   return (
-    <div>
+    <div className="container">
       <h1>All Products</h1>
       {props.products.map((product) => (
         <div key={product.id}>
+          <img className="product_image" src={product.images[0].url} width={250} height={350} alt="" />
           <h2>{product.name}</h2>
-          <img src={product.url} />
-          <p>Width: {product.width}</p>
+          <h4>Price: {product.price}</h4>
+          <p>Description: {product.description}</p>
+          <a href="/has-many-through/products/" class="btn btn-info">
+            More Info
+          </a>
           <p>Height: {product.height}</p>
         </div>
       ))}
