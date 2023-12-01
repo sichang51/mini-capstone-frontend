fa0966dcf32f111dda070d67deb794a452992099
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-11-30 19:39:50 -0800

    added nav bar code to header.jsx and created signup.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,41 @@
 export function Header() {
   return (
-    <header>
-      <nav>
-        <a href="#">Home</a> || <a href="#">Signup</a> || <a href="#">Login</a>
-      </nav>
-    </header>
+    <nav className="navbar navbar-expand-lg" style={{ backgroundColor: "#0d6efd" }} data-bs-theme="dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <Link to="/signup" className="nav-link" style={{ textDecoration: "none" }}>
+                Signup
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/login" className="nav-link" style={{ textDecoration: "none" }}>
+                Login
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
