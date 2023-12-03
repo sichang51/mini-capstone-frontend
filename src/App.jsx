516ac7db12cec20f194c833bb01c072a2e8708d0
sichang51 <99237899+sichang51@users.noreply.github.com>
2023-12-03 14:19:18 -0800

    added toast/flash message on confirmation of signup in App.jsx and Signup.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,25 @@ import { Header } from "./Header";
 import { Content } from "./Content";
 import { Footer } from "./Footer";
 import { BrowserRouter } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [flashMessage, setFlashMessage] = useState("");
+
+  useEffect(() => {
+    if (localStorage.flashMessage) {
+      setFlashMessage(localStorage.flashMessage);
+      localStorage.removeItem("flashMessage");
+    }
+  }, []);
+
   return (
     <div>
       <BrowserRouter>
         <Header />
+        <div onClick={() => setFlashMessage(null)} className="alert alert-success" role="alert">
+          {flashMessage}
+        </div>
         <Content />
         <Footer />
       </BrowserRouter>
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -14,6 +14,7 @@ export function Signup() {
       .then((response) => {
         console.log(response.data);
         event.target.reset();
+        localStorage.setItem("flashMessage", "Thank you for signing up.  Your user account has been created.");
         window.location.href = "/"; // Change this to hide a modal, redirect to a specific page, etc.
       })
       .catch((error) => {
